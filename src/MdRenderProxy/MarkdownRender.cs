899a92f11e696219f7aa985e67080c09b736e525
EvilBeaver <ovsiankin.aa@gmail.com>
2018-01-03 12:29:25 +0300

    Начальный коммит

---
+++ src/MdRenderProxy/MarkdownRender.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Markdig;
+using ScriptEngine.Machine.Contexts;
+
+namespace MdRenderProxy
+{
+    [ContextClass("ПарсерРазметкиMD","MarkdownParser")]
+    public class MarkdownRender : AutoContext<MarkdownRender>
+    {
+        private MarkdownPipeline _parser;
+
+        [ContextProperty("ВключитьРасширения", "EnableExtensions")]
+        public bool EnableExtensions { get; set; }
+
+        [ContextMethod("ПодготовитьПарсер", "PrepareParser")]
+        public void PrepareParser()
+        {
+            var builder = new MarkdownPipelineBuilder();
+            if (EnableExtensions)
+                builder.UseAdvancedExtensions();
+
+            _parser = builder.Build();
+        }
+
+        [ContextMethod("СоздатьHTML","CreateHTML")]
+        public string CreateHtml(string markdownText)
+        {
+            if(_parser == null)
+                PrepareParser();
+
+            var result = Markdown.ToHtml(markdownText, _parser);
+
+            return result;
+        }
+
+        [ScriptConstructor]
+        public static MarkdownRender Constructor()
+        {
+            return new MarkdownRender() { EnableExtensions = true };
+        }
+    }
+}
